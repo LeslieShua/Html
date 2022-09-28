9f0fbe481ea0b96d25eafa3ef46233b121a5682f
LeslieShua <southenm@gmail.com>
2022-09-28 11:56:00 +0900

    'eamil'

---
+++ java.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'ariel';
+   const templateID = 'template_o8wv67j';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('입력하신 정보가 성공적으로 발송되었습니다!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
